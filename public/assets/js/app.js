43be15f548fbd9aa8c936f7390751272dd531e10
tiana <tiana.randrianarivony@gmail.com>
2019-03-20 15:24:24 +0300

    INTEGRATION TEMPLATE

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,5 +3,11 @@ $(function()
     $("[data-isotope-nav] ul li").on("click",function(e){
         var description = $(this).find("span.categorie-description").html();
         $("#categorie-description").html(description);
-    })
+    });
+    let wrapper = document.querySelector( ".description_article" );
+    let options = {
+        // Options go here
+        height: 250
+    };
+    new Dotdotdot( wrapper, options );
 })
